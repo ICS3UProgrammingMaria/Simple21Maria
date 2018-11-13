0317414d5cb13836f17d436293630130dcc3fa61
Maria T <36854099+MariaT7@users.noreply.github.com>
2018-11-13 17:26:58 -0500

    Initial commit

---
+++ Simple21Maria/Simple21Form.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Simple21Maria
+{
+    partial class frmSimple21
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSimple21));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnHit = new System.Windows.Forms.Button();
+            this.btnStay = new System.Windows.Forms.Button();
+            this.mnuFile = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblPlayerAnswer = new System.Windows.Forms.Label();
+            this.lblDealerAnswer = new System.Windows.Forms.Label();
+            this.mnuFile.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(300, 261);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(120, 44);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.UseWaitCursor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnHit
+            // 
+            this.btnHit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHit.Location = new System.Drawing.Point(496, 427);
+            this.btnHit.Name = "btnHit";
+            this.btnHit.Size = new System.Drawing.Size(90, 33);
+            this.btnHit.TabIndex = 1;
+            this.btnHit.Text = "Hit";
+            this.btnHit.UseVisualStyleBackColor = true;
+            // 
+            // btnStay
+            // 
+            this.btnStay.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStay.Location = new System.Drawing.Point(137, 427);
+            this.btnStay.Name = "btnStay";
+            this.btnStay.Size = new System.Drawing.Size(87, 33);
+            this.btnStay.TabIndex = 2;
+            this.btnStay.Text = "Stay";
+            this.btnStay.UseVisualStyleBackColor = true;
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mnuFile.Location = new System.Drawing.Point(0, 0);
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(659, 24);
+            this.mnuFile.TabIndex = 4;
+            this.mnuFile.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.mniExit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(152, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // lblPlayerAnswer
+            // 
+            this.lblPlayerAnswer.AutoSize = true;
+            this.lblPlayerAnswer.Location = new System.Drawing.Point(297, 360);
+            this.lblPlayerAnswer.Name = "lblPlayerAnswer";
+            this.lblPlayerAnswer.Size = new System.Drawing.Size(0, 13);
+            this.lblPlayerAnswer.TabIndex = 5;
+            // 
+            // lblDealerAnswer
+            // 
+            this.lblDealerAnswer.AutoSize = true;
+            this.lblDealerAnswer.Location = new System.Drawing.Point(397, 360);
+            this.lblDealerAnswer.Name = "lblDealerAnswer";
+            this.lblDealerAnswer.Size = new System.Drawing.Size(0, 13);
+            this.lblDealerAnswer.TabIndex = 6;
+            // 
+            // frmSimple21
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Simple21Maria.Properties.Resources.live_casino9;
+            this.ClientSize = new System.Drawing.Size(659, 500);
+            this.Controls.Add(this.lblDealerAnswer);
+            this.Controls.Add(this.lblPlayerAnswer);
+            this.Controls.Add(this.btnStay);
+            this.Controls.Add(this.btnHit);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.mnuFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmSimple21";
+            this.Text = "Simple Blackjack";
+            this.mnuFile.ResumeLayout(false);
+            this.mnuFile.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnHit;
+        private System.Windows.Forms.Button btnStay;
+        private System.Windows.Forms.MenuStrip mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.Label lblPlayerAnswer;
+        private System.Windows.Forms.Label lblDealerAnswer;
+    }
+}
+
---
+++ Simple21Maria/Simple21Form.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simple21Maria
+{
+    public partial class frmSimple21 : Form
+    { //lblAnswer.Hide();
+        public frmSimple21()
+        {
+            InitializeComponent();
+           
+
+        }
+
+        //Declare global variables
+        int player = 0; int dealer = 0; int totaldealer; int totalplayer = 1;
+        const int MAX_VALUE = 10, MIN_VALUE = 1;
+
+
+        Random cards = new Random();
+
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Hide();
+
+
+            player = cards.Next(MAX_VALUE, MIN_VALUE + 1) * 2;
+            dealer = cards.Next(MAX_VALUE, MIN_VALUE + 1);
+
+            lblPlayerAnswer.Text = Convert.ToString(player);
+           lblDealerAnswer.Text = Convert.ToString(dealer);
+
+
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var form1 = new frmSimple21())
+            {
+                Visible = false;
+                form1.ShowDialog();
+                Close();
+            }
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
